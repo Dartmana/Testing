517cb390630699d4970f7bb8bc36c5d44ddb6c88
Raveblade7 <mikhail.artyomenko@gmail.com>
2024-09-25 10:37:04 -0600

    Test

---
+++ Test.py
@@ -0,0 +1,13 @@
+
+birth_year = input("Enter your birth year: ")
+age = 2024 - int(birth_year)
+print("You are", age , "years old")
+
+# day = input("How is your day today?")
+num = 1
+if num > 2:
+    print(num ,"is greater than two!")
+else:
+    print("Your ok")
+
+
